2bc9748023c09ba86341daedc074fd3a9a047c7b
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-05-17 00:47:33 +0300

    Updated main.go

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
 func main() {
 
-	server := NewServer("127.0.0.1:8080")
+	dataStore, err := NewPostgresStore()
+
+	if err != nil {
+		log.Fatalf("An error occurred while trying to connect to database: %v\n", err.Error())
+	}
+
+	fmt.Printf("%+v\n", dataStore)
+
+	server := NewServer(":8080", dataStore)
 	server.Run()
+
 }
