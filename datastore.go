d3140b074d581e941d478b644dc3d6083a6c153c
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-05-18 19:02:29 +0300

    Updated datastore.go * added functions createAccountTable() and Init() to create account table in database * implemented functions CreateAccount() and GetAllAccounts for struct PostgresStore

--- datastore.go
+++ datastore.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"log"
+
 	_ "github.com/lib/pq"
 )
 
@@ -9,6 +11,7 @@ type DataStore interface {
 	CreateAccount(acc *Account) error
 	DeleteAccount(id int) error
 	UpdateAccount(id int) error
+	GetAllAccounts() ([]*Account, error)
 	GetAccountByID(id int) (*Account, error)
 }
 
@@ -34,7 +37,41 @@ func NewPostgresStore() (*PostgresStore, error) {
 
 }
 
+func (s *PostgresStore) Init() error {
+	return s.createAccountTable()
+}
+
+func (s *PostgresStore) createAccountTable() error {
+
+	query := `CREATE TABLE IF NOT EXISTS account (
+		id SERIAL PRIMARY KEY,
+		first_name VARCHAR(255),
+		last_name VARCHAR(255),
+		number SERIAL,
+		balance INT,
+		created_at TIMESTAMP
+	);`
+
+	_, err := s.db.Exec(query)
+
+	return err
+
+}
+
 func (s *PostgresStore) CreateAccount(acc *Account) error {
+
+	query := `
+	INSERT INTO account (first_name, last_name, number, balance, created_at)
+	VALUES ($1, $2, $3, $4, $5);`
+
+	rows, err := s.db.Query(query, acc.FirstName, acc.LastName, acc.Number, acc.Balance, acc.CreatedAt)
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("%+v\n", rows)
+
 	return nil
 }
 
@@ -48,4 +85,38 @@ func (s *PostgresStore) UpdateAccount(id int) error {
 
 func (s *PostgresStore) GetAccountByID(id int) (*Account, error) {
 	return nil, nil
+}
+
+func (s *PostgresStore) GetAllAccounts() ([]*Account, error) {
+
+	query := `SELECT * FROM account`
+
+	rows, err := s.db.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	accounts := []*Account{}
+
+	for rows.Next() {
+		account := &Account{}
+
+		err := rows.Scan(
+			&account.ID,
+			&account.FirstName,
+			&account.LastName,
+			&account.Number,
+			&account.Balance,
+			&account.CreatedAt)
+
+		if err != nil {
+			return nil, err
+		}
+
+		accounts = append(accounts, account)
+
+	}
+
+	return accounts, nil
 }
