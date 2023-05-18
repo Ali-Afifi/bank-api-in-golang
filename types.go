05784e145648f0c5860f0dcd3de074db63b09194
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-05-18 18:59:22 +0300

    Updated types.go * added a new timestamp field to Account struct * added new struct CreateAccountRequestBody

--- types.go
+++ types.go
@@ -1,21 +1,32 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 type Account struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Number    int64  `json:"number"`
-	Balance   int64  `json:"balance"`
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Number    int64     `json:"number"`
+	Balance   int64     `json:"balance"`
+	CreatedAt time.Time `json:"createdAt"`
 }
+
+type CreateAccountRequestBody struct {
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+}
+
 
 func NewAccount(firstName, lastName string) *Account {
 	return &Account{
-		ID:        rand.Intn(1000000),
+		ID:        0,
 		FirstName: firstName,
 		LastName:  lastName,
 		Number:    rand.Int63n(1000000000),
+		CreatedAt: time.Now().UTC(),
 	}
 }
 
