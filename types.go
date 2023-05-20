710c3c18e6474a0cf19161706c98c8470ddf7921
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-05-20 15:31:13 +0300

    Updated types.go

--- types.go
+++ types.go
@@ -9,23 +9,22 @@ type Account struct {
 	ID        int       `json:"id"`
 	FirstName string    `json:"firstName"`
 	LastName  string    `json:"lastName"`
-	Number    int64     `json:"number"`
-	Balance   int64     `json:"balance"`
+	Number    int       `json:"number"`
+	Balance   int       `json:"balance"`
 	CreatedAt time.Time `json:"createdAt"`
 }
 
 type CreateAccountRequestBody struct {
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
 }
-
 
 func NewAccount(firstName, lastName string) *Account {
 	return &Account{
 		ID:        0,
 		FirstName: firstName,
 		LastName:  lastName,
-		Number:    rand.Int63n(1000000000),
+		Number:    rand.Intn(1000000000),
 		CreatedAt: time.Now().UTC(),
 	}
 }
